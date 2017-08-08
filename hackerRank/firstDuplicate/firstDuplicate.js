e5f0263424bb42ba08c05b9122bad7c6d7a945fb
SiamKing <thesiameseelectric@gmail.com>
2017-08-07 19:17:08 -0700

    refactored firstDuplicate.js - still needs to be faster

--- hackerRank/firstDuplicate/firstDuplicate.js
+++ hackerRank/firstDuplicate/firstDuplicate.js
@@ -1,21 +1,15 @@
 function firstDuplicate(a) {
     let lowIndex = [],
-        index,
         currArr,
         i;
 
     for (i = 0; i < a.length - 1; i++) {
         if (i >= lowIndex[0]) {
-            break;
+            return a[lowIndex[0]];
         }
-        currArr = a.slice(i + 1)
-        if (currArr.indexOf(a[i]) > -1) {
-            index = currArr.indexOf(a[i]) + i + 1;
-            if (index < lowIndex[0] || lowIndex.length == 0) {
-                lowIndex.pop();
-                lowIndex.push(index)
-                // a.splice(0, index);
-            }
+        currArr = a.slice(i + 1, lowIndex[0])
+        if (currArr.includes(a[i])) {
+            lowIndex[0] = currArr.indexOf(a[i]) + i + 1;
         }
     }
     return lowIndex.length ? a[lowIndex[0]] : -1;
