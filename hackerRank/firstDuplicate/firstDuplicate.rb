67ca2e28fd237e9cdc809c77ac85906ba4e9a940
SiamKing <thesiameseelectric@gmail.com>
2017-08-08 10:42:31 -0700

    finished the ruby version of firstDuplicate

--- hackerRank/firstDuplicate/firstDuplicate.rb
+++ hackerRank/firstDuplicate/firstDuplicate.rb
@@ -1,19 +1,11 @@
-def firstDuplicate(a)
-    let lowIndex = [],
-        i,
-        j;
+require 'set'
 
-    for (i = 0; i < a.length; i++) {
-        if (lowIndex[0] < arr[i]) {
-            return lowIndex[0];
-        }
-        for (j = i + 1; j < a.length; j++) {
-            if (arr[i] == arr[j]) {
-                lowIndex.pop()
-                lowIndex.push(j);
-                break;
-            }
-        }
-    }
-    return -1;
+def firstDuplicate(a)
+    mySet = Set.new
+
+    for i in a do
+      return i if mySet.include?(i)
+      mySet << i
+    end
+    -1
 end
